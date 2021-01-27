862dfd656f0b9d73a890f87b8efae76d0a330293
GlebVetrov <5925520@gmail.com>
2021-01-27 23:17:18 +0300

    feat: add some title component for project

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -17,3 +17,22 @@ export const Subtitle = styled.div`
   font-size: 12px;
   margin-bottom: 8px;
 `;
+
+export const Subject = styled.span`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 24px;
+  color: #FFFFFF;
+`;
+
+export const NavigationTitle = styled.h3`
+  font-weight: bold;
+  font-size: 11px;
+  line-height: 12px;
+  text-transform: uppercase;
+  color: #FFFFFF;
+  
+  .collapse & {
+    display: none;
+  }
+`;
