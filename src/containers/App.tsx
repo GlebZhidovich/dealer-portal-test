659fa0fe9fb9c81e931b36b15616cb0362d01488
GlebVetrov <5925520@gmail.com>
2021-01-27 23:18:57 +0300

    feat: add global styles for project

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,6 +2,20 @@ import React, { Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import MainLayout from './MainLayout';
 import { ROUTES } from '../constants';
+import { createGlobalStyle } from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`  
+  * {
+    box-sizing: border-box;
+  }
+  
+  body {
+    font-family: SF Pro Text, sans-serif;
+    font-style: normal;
+  }
+`;
+
 
 const OldScreen = React.lazy(() => import('./OldScreen'));
 
@@ -9,6 +23,7 @@ function App() {
   return (
     <Suspense fallback={<p>loading</p>}>
       <MainLayout>
+        <GlobalStyle/>
         <Switch>
           <Route exact path={ROUTES.sites}>
             <OldScreen />
